41d0dc40e86b56ac38466f68f69c01acb8a95a83
Alex Harrison <alexander.harrison.r@gmail.com>
2025-05-29 18:08:04 -0600

    change port to work with render.com

--- src/server.c
+++ src/server.c
@@ -142,7 +142,7 @@ int main(void) {
 
     struct mg_mgr mgr;
     mg_mgr_init(&mgr);
-    mg_http_listen(&mgr, "http://0.0.0.0:8000", ev_handler, NULL);
+    mg_http_listen(&mgr, "http://0.0.0.0:10000", ev_handler, NULL);
     while (true) {
         mg_mgr_poll(&mgr, -1);
     }
